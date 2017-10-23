1f8218714272047beb562b45d1ea99ec7d5177a4
Laine <T3CHNOLOG1C@users.noreply.github.com>
2017-10-23 08:25:15 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 
 # Brainfug by T3CHNOLOG1C
-# license: WTFPL
+# license: Apache License 2.0
 # https://github.com/T3CHNOLOG1C/Brainfug
-
-description = """
-Brainfug, a bot for interpreting Brainfuck code!
-"""
+# Based off of Kurisu
+# https://github.com/ihaveamac/Kurisu
 
 # import dependencies
-import os
-from discord.ext import commands
-import discord
-import datetime
-import json, asyncio
+import asyncio
 import copy
 import configparser
-import traceback
-import sys
+import datetime
+import imp
+import json
 import os
 import re
+import sys
+import traceback
+import discord
+from discord.ext import commands
+
+
+description = """
+Brainfug, a bot for interpreting Brainf*ck code!
+"""
 
 # sets working directory to bot's folder
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -34,24 +38,64 @@ bot = commands.Bot(command_prefix=prefix, description=description, pm_help=None)
 
 # http://stackoverflow.com/questions/3411771/multiple-character-replace-with-python
 chars = "\\`*_<>#@:~"
+
+
 def escape_name(name):
     name = str(name)
     for c in chars:
         if c in name:
             name = name.replace(c, "\\" + c)
     return name.replace("@", "@\u200b")  # prevent mentions
+
+
 bot.escape_name = escape_name
+
+bot.pruning = False  # used to disable leave logs if pruning, maybe.
+
 
 # mostly taken from https://github.com/Rapptz/discord.py/blob/async/discord/ext/commands/bot.py
 @bot.event
 async def on_command_error(error, ctx):
     if isinstance(error, commands.errors.CommandNotFound):
-        pass  # ...don't need to know if commands don't exist
+
     elif isinstance(error, commands.errors.MissingRequiredArgument):
         formatter = commands.formatter.HelpFormatter()
         await bot.send_message(ctx.message.channel, "{} You are missing required arguments.\n{}".format(ctx.message.author.mention, formatter.format_help_for(ctx, ctx.command)[0]))
+
+    elif isinstance(error, commands.errors.CommandOnCooldown):
+        try:
+            await bot.delete_message(ctx.message)
+        except discord.errors.NotFound:
+            pass
+        message = await bot.send_message(ctx.message.channel, "{} This command was used {:.2f}s ago and is on cooldown. Try again in {:.2f}s.".format(ctx.message.author.mention, error.cooldown.per - error.retry_after, error.retry_after))
+        await asyncio.sleep(10)
+        await bot.delete_message(message)
+
     else:
-        await bot.send_message(ctx.message.channel, "An error occured while processing the `{}` command.".format(ctx.command.name))
+      pass
+
+bot.all_ready = False
+bot._is_all_ready = asyncio.Event(loop=bot.loop)
+async def wait_until_all_ready():
+    """Wait until the entire bot is ready."""
+    await bot._is_all_ready.wait()
+bot.wait_until_all_ready = wait_until_all_ready
+
+@bot.event
+async def on_ready():
+    # this bot should only ever be in one server anyway
+    for server in bot.servers:
+        bot.server = server
+        if bot.all_ready:
+            break
+
+        print("{} has started! {} has {:,} members!".format(bot.user.name, server.name, server.member_count))
+
+
+        bot.all_ready = True
+        bot._is_all_ready.set()
+
+        break
 
 # loads extensions
 addons = [
